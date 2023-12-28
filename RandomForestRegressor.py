03e4272a26e041d26eb5c83d171919dfa535908f
ammarelbadry1 <ammar.b6507187@gmail.com>
2023-12-28 12:49:20 +0200

    [edit] evaluation presentation

--- RandomForestRegressor.py
+++ RandomForestRegressor.py
@@ -13,10 +13,10 @@ X = df.drop("RUL", axis=1).drop("Cycle_Index", axis=1)
 y = df["RUL"]
 
 ## Split the data
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
 
 # Train the model
-model = RandomForestRegressor(n_estimators=100, random_state=42, oob_score=True)
+model = RandomForestRegressor(n_estimators=20, oob_score=True)
 model.fit(X_train, y_train)
 
 
@@ -30,7 +30,7 @@ r2 = r2_score(y_test, predictions)
 
 print('Model Evaluation:')
 print(f'Out-of-Bag Score: {oob_score}')
-print(f'R2 Score: {r2}')
+print(f'R2 Score: {r2 * 100:.2f}%')
 print()
 
 importances = model.feature_importances_
