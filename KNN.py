85def43c1dd3ae36d325703477a2bfda9cf89691
ammarelbadry1 <ammar.b6507187@gmail.com>
2023-12-28 12:44:32 +0200

    [add] model visualization

--- KNN.py
+++ KNN.py
@@ -1,6 +1,6 @@
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_squared_error, r2_score
 import matplotlib.pyplot as plt
 import pandas as pd
 
@@ -38,13 +38,15 @@ knn.fit(X_train, y_train)
 y_pred = knn.predict(X_test)
 
 mse = mean_squared_error(y_test, y_pred)
+r2 = r2_score(y_test, y_pred)
 print("Mean squared error:", mse)
+print("r2 score: ", r2)
 
-feature_to_plot1 = "Max. Voltage Dischar. (V)"
 
-plt.scatter(X_test[feature_to_plot1], y_test, label="Actual target")
-plt.scatter(X_test[feature_to_plot1], y_pred, label="Predicted target")
-plt.xlabel(feature_to_plot1)
-plt.ylabel("RUL")
+plt.plot(y_test, y_test, color='blue')
+plt.scatter(y_test, y_pred, color='red', s=3)
+plt.title("Test Values VS Prediction Values")
+plt.xlabel("True Values")
+plt.ylabel("Prediction Values")
 plt.legend()
 plt.show()
