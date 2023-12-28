f7cb12881e11b0de247494d1309c4988cb411918
adelhassan1 <tomasander12@gmail.com>
2023-12-28 07:33:11 +0200

    SVR

--- SupportVectorRegression.py
+++ SupportVectorRegression.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.svm import SVR
+from sklearn.metrics import r2_score
 from sklearn.preprocessing import StandardScaler
 import os
 
@@ -13,12 +14,6 @@ file_path = os.path.join(current_directory, 'Battery_RUL.csv')
 
 # Load the dataset
 dataset = pd.read_csv(file_path)
-
-# print(dataset.shape)
-# print("##########################################")
-# print(dataset.columns)
-# print("##########################################")
-# print(dataset.head())
 
 # Define the features and target
 features = ['Cycle_Index', 'Discharge Time (s)', 'Decrement 3.6-3.4V (s)',
@@ -37,8 +32,18 @@ X = sc_X.fit_transform(X)
 y = sc_y.fit_transform(y.reshape(-1, 1)).ravel()
 
 # SVR model
+# SVR model
 regressor = SVR(kernel='rbf')
+
+# Fit the model
 regressor.fit(X, y)
+
+# Predictions using the original feature set
+y_pred_orig = regressor.predict(X)
+
+# Calculate R^2 score
+r2 = r2_score(y, y_pred_orig)
+
 
 # Predictions using entire scaled feature set
 X_pred = np.linspace(-2, 2, 1000).reshape(-1, len(features))  # Change -2 and 2 based on your data range
@@ -58,3 +63,14 @@ plt.xlabel('Cycle_Index')  # Change this label based on the feature you're plott
 plt.ylabel('RUL')
 plt.legend()
 plt.show()
+
+# Calculate the errors
+errors = y - y_pred_orig
+
+# Plot the error distribution
+plt.hist(errors, bins=30)
+plt.xlabel('Prediction Error')
+plt.ylabel('Count')
+plt.title('Distribution of Prediction Errors')
+plt.show()
+print(f'Accuracy: {r2 * 100:.2f}%')
